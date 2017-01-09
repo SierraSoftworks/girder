e8de001087b4fddef2754d5ae648899bbf99b9e0
Benjamin Pannell <bpannell@demonware.net>
2017-01-09 10:50:41 +0000

    feat: Add tools to modify error messages easily

--- errors/errors.go
+++ errors/errors.go
@@ -14,6 +14,39 @@ func (e *Error) Error() string {
 	return fmt.Sprintf("%d %s: %s", e.Code, e.Name, e.Message)
 }
 
+// WithCode allows you to create a new error object which inherits the properties
+// of its source, with a different error code.
+func (e *Error) WithCode(code int) *Error {
+	return &Error{
+		Code:       code,
+		Name:       e.Name,
+		Message:    e.Message,
+		Stacktrace: e.Stacktrace,
+	}
+}
+
+// WithName allows you to create a new error object which inherits the properties
+// of its source, with a different error name.
+func (e *Error) WithName(name string) *Error {
+	return &Error{
+		Code:       e.Code,
+		Name:       name,
+		Message:    e.Message,
+		Stacktrace: e.Stacktrace,
+	}
+}
+
+// WithMessage allows you to create a new error object which inherits the properties
+// of its source, with a different error message.
+func (e *Error) WithMessage(message string) *Error {
+	return &Error{
+		Code:       e.Code,
+		Name:       e.Name,
+		Message:    message,
+		Stacktrace: e.Stacktrace,
+	}
+}
+
 // NewError creates a new API level error object with the given properties
 func NewError(code int, name, message string) *Error {
 	return &Error{
